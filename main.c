3f367ba92467a817a0fd1abeb734b50787ea5f66
Hrushikesh Dharmadhikari <87522414+HrushikeshDharmadhikari@users.noreply.github.com>
2022-01-12 14:17:19 +0530

    Update 1.1
    Added loop functionality to loggedOut()

--- main.c
+++ main.c
@@ -1,57 +1,30 @@
 #include "stdio.h"
 #include "strings.h"
 #include "stdlib.h"
-FILE *logstat, *prefs, *file, *accounts;
+FILE *logstat, *file, *accounts;
 char filenam[500];
-char name[100];
-char nemail[50];
-char temp[10];
-char tempname[50];
 int logged, logaction;
 struct credencials
 {
     char logemail[50];
     int pin;
 }cred;
-void main()
+struct newAccount
 {
+    char nemail[100];
+    char name[100];
+    int npin;
+}newAcc;
+struct tempAcc
+{
+    char logemail[100];
+    char name[100];
+    int pin;
+}tempAcc;
 
-	logstat = fopen("files/logstat.dat", "rb+");
-	accounts = fopen("files/accounts.dat", "ab+");
-	prefs = fopen("files/prefs.dat", "rb+");
+//If logged out this will try to log you in.
 
-	if(logstat==NULL)
-	{
-		printf("Error acessing logstat");
-		exit(0);
-	}
-	if(accounts==NULL)
-	{
-		printf("Error acessing accounts");
-		exit(0);
-	}
-	if(prefs==NULL)
-	{
-		printf("Error acessing prefs");
-		exit(0);
-	}
-
-	fscanf(logstat, "%d", &logged);
-
-	if(logged == 0)
-	{
-		lout();
-	}
-	if(logged == 1)
-	{
-		lind();
-	}
-
-	fclose(logstat);
-	fclose(accounts);
-	fclose(prefs);
-}
-void lout()
+void loggedOut()
 {
 	char mail[50];
 	int logpin;
@@ -60,36 +33,47 @@ void lout()
 
 	if(logaction == 1)
 	{
-		printf("\nEnter email: ");
-		scanf("%s", mail);
-		fseek(accounts, 0, SEEK_SET);
+        fclose(accounts);
+		fopen("files/accounts.dat", "rb+");
+		int login = 0;
+        while(login == 0)
+        {
+            printf("\nEnter email and pin: ");
+            scanf("%s %d", cred.logemail, &cred.pin);
+            while(fread(&tempAcc, sizeof(tempAcc), 1, accounts)!=NULL)
+            {
+                if(strcmp(cred.logemail, tempAcc.logemail)==0)
+                {
+                    if(tempAcc.pin==cred.pin)
+                    {
+                        login = 1;
+                        loggedIn();
+                    }
 
-		printf("Invalid email...Press any key to exit...");
-		getch();
-		exit(0);
+                }
+            }
+            printf("\nInvalid ID");
+        }
+        getch();
+        exit(0);
 	}
-	if(1)
-
 
 	if(logaction == 2)
 	{
-		printf("Enter name, email, PIN: ");
-		scanf("");
+		printf("\nEnter email, name and pin: ");
+		scanf("%s %s %d", newAcc.nemail, newAcc.name, &newAcc.npin);
+		fwrite(&newAcc, sizeof(newAcc), 1, accounts);
 
-		fclose(logstat);
-		logstat=fopen("files/logstat.dat", "wb+");
-		fprintf(logstat, "1");
-		fclose(accounts);
-		fclose(logstat);
-		names = fopen("files/names.dat", "rb+");
-		lind();
+		loggedIn();
 	}
 }
- void lind()
+
+//Main NotePad code
+
+void loggedIn()
 {
 	char lines[100];
 	int log;
-
 	printf("Hello! Welcome to notepad. Enter 1 to open file, enter 2 to logout: ");
 	scanf("%d", &log);
 
@@ -99,7 +83,7 @@ void lout()
 		logstat = fopen("files/logstat.dat", "w+");
 		logged=0;
 		fprintf(logstat, "%d",logged);
-		lout();
+		loggedOut();
 	}
     if(log==1)
     {
@@ -125,15 +109,38 @@ void lout()
 	fclose(file);
 }
 
+//All the code that runs to check everything is fine and also initialises files and error checks.
 
+void main()
+{
 
+	logstat = fopen("files/logstat.dat", "rb+");
+	accounts = fopen("files/accounts.dat", "ab+");
 
+	if(logstat==NULL)
+	{
+		printf("Error acessing logstat");
+		getch();
+		exit(0);
+	}
+	if(accounts==NULL)
+	{
+		printf("Error acessing accounts");
+		getch();
+		exit(0);
+	}
+	fscanf(logstat, "%d", &logged);
 
+	if(logged == 0)
+	{
+		loggedOut();
+	}
+	if(logged == 1)
+	{
+		loggedIn();
+	}
 
-
-
-
-
-
-
-
+	fclose(logstat);
+	fclose(accounts);
+	fclose(file);
+}
